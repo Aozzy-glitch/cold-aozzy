413be3d0df3e0a97e36dbeb335ca8e32a5209f38
Aozzy-glitch <amabdi9867@gmail.com>
2024-06-15 06:31:27 -0400

    Cold-Aozzy

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -7,8 +7,13 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
+      <div className='myimage'>
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <h1>Hello and Welcome to Cold-Aozzy!</h1>
+      <p> Welcome to Cold-Aozzy the number 1 rated Ai cold calling service for your agency.Signup today for free!</p>
+      <button className='button'>Sign up</button>
+      </div>
+      
     </div>
   );
 };
