f0eb6eef628a53631f5b6ab6d60e4be33862a5d2
Aozzy-glitch <amabdi9867@gmail.com>
2024-06-19 05:09:08 -0400

    Cold-Aozzy

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonInput } from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -7,35 +6,53 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Coding</IonTitle>
+          <IonTitle slot="start">GoCode</IonTitle>
+          <IonButtons slot="end" className="toolbar-buttons">
+            <IonButton>Courses</IonButton>
+            <IonButton>Pricing</IonButton>
+            <IonButton>Contact Us</IonButton>
+            <IonInput placeholder="Search" type="text" className="search-input" />
+            <IonButton>Login</IonButton>
+            <IonButton>Sign Up</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <div className='mycontainer'>
           <div className='nopp'></div>
           <div className='containerinacontainer'>
-      
-              <div className='myinnercontainer'>
-  
-              <h1 className='h12'>Learn Coding
-              <br/>
-              Anytime</h1>
-              <p>Designed for kids and Teens</p>
-
-
-              <button className='btn12'>Get started &nbsp; &#8594;</button>
-        
-            </div>
-
             <div className='myinnercontainer'>
-            <img src='https://parachictacademy.com.ng/wp-content/uploads/2022/04/coding-classes-for-kids-in-Ibadan.png'/>
+              <h1 className='h12'>Learn Coding
+                <br/>
+                Anytime
+              </h1>
+              <p>Designed for kids and Teens</p>
+              <button className='btn12'>Get started &nbsp; &#8594;</button>
             </div>
-          
-
+            <div className='myinnercontainer'>
+              <img src='https://parachictacademy.com.ng/wp-content/uploads/2022/04/coding-classes-for-kids-in-Ibadan.png' alt='Coding classes for kids'/>
             </div>
-            
-
-
+          </div>
+        </div>
+        <div className='second-page'>
+          <h1 className='why-title'>Why <span className='gocode'>GoCode</span></h1>
+          <div className='features-container'>
+            <div className='feature'>
+              <img src='/mnt/data/gocode2.PNG' alt='Personalized Course' />
+              <h2>Personalized Course</h2>
+              <p>Students learn and practice at their own pace.</p>
+            </div>
+            <div className='feature'>
+              <img src='/mnt/data/gocode3.PNG' alt='Trusted Teacher' />
+              <h2>Trusted Teacher</h2>
+              <p>We only choose the best and verified teachers.</p>
+            </div>
+            <div className='feature'>
+              <img src='/mnt/data/gocode3.PNG' />
+              <h2>Large Community</h2>
+              <p>We provide forum area, so students can help each other.</p>
+            </div>
+          </div>
         </div>
       </IonContent>
     </IonPage>
